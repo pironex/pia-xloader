b7076a92207b3f2ed96905d5caf9428b2cc7e95d
Bjoern Krombholz <b.krombholz@pironex.de>
2011-12-05 15:23:50 +0100

    pia35x: change default boot to try MMC before NAND
    - X-Loader did choose the wrong boot mode when peripheral boot was enabled,
      so booting from MMC was never possible, when using a SYS_BOOT config that
      included NAND
    
    - now we try to init MMC first
      if it fails we try booting from NAND
      if MMC init was successful we boot from MMC

--- lib/board.c
+++ lib/board.c
@@ -77,21 +77,22 @@ void start_armboot (void)
 	buf =  (uchar*) CFG_LOADADDR;
 
 	int i;
-	if ((get_mem_type() == MMC_ONENAND) || (get_mem_type() == MMC_NAND)){
-		buf += mmc_boot(buf);
-	}
+	buf += mmc_boot(buf);
 
-	if (get_mem_type() == GPMC_ONENAND){
-		for (i = ONENAND_START_BLOCK; i < ONENAND_END_BLOCK; i++){
-			if (!onenand_read_block(buf, i))
-				buf += ONENAND_BLOCK_SIZE;
+	/* only boot from nand if mmc boot failed */
+	if (buf == (uchar *)CFG_LOADADDR) {
+		if (get_mem_type() == GPMC_ONENAND){
+			for (i = ONENAND_START_BLOCK; i < ONENAND_END_BLOCK; i++){
+				if (!onenand_read_block(buf, i))
+					buf += ONENAND_BLOCK_SIZE;
+			}
 		}
-	}
 
-	if (get_mem_type() == GPMC_NAND){
-		for (i = NAND_UBOOT_START; i < NAND_UBOOT_END; i+= NAND_BLOCK_SIZE){
-			if (!nand_read_block(buf, i))
-				buf += NAND_BLOCK_SIZE; /* advance buf ptr */
+		if (get_mem_type() == GPMC_NAND){
+			for (i = NAND_UBOOT_START; i < NAND_UBOOT_END; i+= NAND_BLOCK_SIZE){
+				if (!nand_read_block(buf, i))
+					buf += NAND_BLOCK_SIZE; /* advance buf ptr */
+			}
 		}
 	}
 #endif
